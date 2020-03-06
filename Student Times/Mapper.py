08cfc1e0d9d4632aed3fff86f5264ae8bdc3307c
Pooja Chourey <46944951+iampoojachourey@users.noreply.github.com>
2020-03-06 00:00:13 -0600

    Mapper.py

---
+++ Student Times/Mapper.py
@@ -0,0 +1,12 @@
+import sys
+import csv
+
+reader = csv.reader(sys.stdin, delimiter='\t')
+
+reader.next()
+
+for line in reader:
+    if len(line) == 19:
+        author_id = line[3]
+        hour = line[8].split(' ')[1].split(':')[0]
+        print "{0}\t{1}".format(author_id, hour)
